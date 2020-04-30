07f1c0cd0b12b84b0d0a22b388599c8dcb431ddf
Dominus <62185555+LunSable@users.noreply.github.com>
2020-04-30 15:36:40 +0500

    Update practika1.php

--- practika1.php
+++ practika1.php
@@ -1,18 +1,18 @@
 <?php
-    $h = "QqWwEeRrTtYyUuIiOoPpAaSsDdFfGgHhJjKkLlZzXxCcVvBbNnMm1234567890"; //Выбираем символы, из которых будет состоять наш рандом
-    $rand = substr(str_shuffle($h), 0, 5); //создаём ранд. Цифра 5 обозначает длину ранда
-    $site = ""; //Адрес вашего сайта. Слэш обязателен!
+    $h = "QqWwEeRrTtYyUuIiOoPpAaSsDdFfGgHhJjKkLlZzXxCcVvBbNnMm1234567890"; 
+    $rand = substr(str_shuffle($h), 0, 5); 
+    $site = ""; 
     $url = $_REQUEST['url'];
     //echo $url;
     if ($_REQUEST['submit']) {
-        echo "<div class='intro_save' id='box' > <div class='intro_textsave'>Вот сокращенная ссылка <a href='$site/a/$rand.php' target='_blank' class='intro_cut'>$site/a/$rand</a><a href='/' class='intro_button_two'>ОК</a></div></div>"; //выводим пользователю ссылку, в виде ссылки
-        $f = fopen("a/$rand.php", "w"); //Создаём файл с именем ранда. Я решил скидывать файлы в каталог a, поэтому не забудьте его создать!
-        fwrite($f, "<?php header('Location: $url') ?>"); //И записываем в код редиректа, с ссылкой которую ввёл пользователь
-        fclose($f); //Закрываем файл
-        $fh = fopen(".htaccess", "a"); //Открываем файл .htaccess с дозаписью на последний байт
+        echo "<div class='intro_save' id='box' > <div class='intro_textsave'>Вот сокращенная ссылка <a href='$site/a/$rand.php' target='_blank' class='intro_cut'>$site/a/$rand</a><a href='/' class='intro_button_two'>ОК</a></div></div>"; 
+        $f = fopen("a/$rand.php", "w"); 
+        fwrite($f, "<?php header('Location: $url') ?>"); 
+        fclose($f); 
+        $fh = fopen(".htaccess", "a"); 
         fwrite($fh, "
-        RewriteRule ^$rand$ /a/$rand.php"); //Записываем ссылку на файл в каталоге a и её сокращённый вариант, который был дан пользователю. !ВНИМАНИЕ! Перенос сделан специально, иначе всё будет писаться в плотную и вызовет 500 ошибку!
-        fclose($fh); //Закрываем файл 
+        RewriteRule ^$rand$ /a/$rand.php"); 
+        fclose($fh); 
     }
 ?>
 <script>
